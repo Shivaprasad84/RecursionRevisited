3a537714800ea360fcde740b3769ffd71891cc2e
Shivaprasad84 <shivaprasadsmys@gmail.com>
2019-07-12 12:06:56 +0530

    Dice Roll

---
+++ diceRolls.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print(vector<int>& vec)
+{
+    cout << "(";
+    for(auto v : vec)
+        cout << v << " ";
+    cout << ")" << endl;
+}
+
+void diceHelper(int dice, vector<int> &chosen)
+{
+    if (dice == 0)
+    {
+        print(chosen);
+    }
+    else
+    {
+        for (int i = 1; i <= 6; i++)
+        {
+            //choose
+            chosen.push_back(i);
+            //explore
+            diceHelper(dice - 1, chosen);
+            //unchoose
+            chosen.pop_back();
+
+        }
+    }
+}
+
+void diceRoll(int dice)
+{
+    vector<int> chosen;
+    diceHelper(dice, chosen);
+}
+
+int main()
+{
+    int dice;
+    cout << "Enter number of die: ";
+    cin >> dice;
+    diceRoll(dice);
+}
